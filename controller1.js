0ef759e4ba33e72c7d56174f6c20ce77fe6dff21
AAKASH HANDA <AAKASH HANDA>
2017-09-18 19:40:05 +0530

    A

--- controller1.js
+++ controller1.js
@@ -1,11 +1,11 @@
 (function (){
 	var  app = angular.module("githubViewer", []);
 
-	var MainController = function($scope, $http, $interval, $log, $anchorScroll, $location){
-		var onUserComplete = function(response){
-			$scope.user = response.data;
-			$http.get($scope.user.repos_url)
-				.then(onRepos, onError)
+	var MainController = function($scope, $http, $interval, $log, $anchorScroll, $location, github){
+		var onUserComplete = function(data){
+			$scope.user = data;
+			//$http.get($scope.user.repos_url)
+			github.getRepos($scope.user).then(onRepos, onError)
 		}
 
 		var onRepos = function(response){
@@ -32,8 +32,8 @@
 
 		$scope.search = function(username){
 			$log.info("searching for "+ username);
-			$http.get("https://api.github.com/users/"+ username)
-				.then(onUserComplete, onError);
+			//$http.get("https://api.github.com/users/"+ username)
+			github.getUser(username).then(onUserComplete, onError);
 			if(countdownInterval){
 				$interval.cancel(countdownInterval);
 				$scope.countdown=null;
